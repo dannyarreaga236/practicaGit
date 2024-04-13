da7b61ec82cfb7f11bc5357a3eab4df469ec8676
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-12 23:42:19 -0600

    Prueba operacion ok

--- pruebas.py
+++ pruebas.py
@@ -7,3 +7,9 @@ ej1.tabla(numero)
 # Probar el area del cuadrado
 lado = int(input('Cuanto mide el lado? '))
 ej1.area_cuadrado(lado)
+
+# Probar las operaciones
+op = int(input('1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\nElige una operacion: '))
+num1 = int(input('Ingresa el primer numero: '))
+num2 = int(input('Ingresa el segundo numero: '))
+ej1.operacion(num1, num2, op)
