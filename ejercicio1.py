17eaa7f54ee5cb5cffaff9754c8c19bd5a24fe4d
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-12 23:33:41 -0600

    Funcion operaciones finalizada

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,3 +4,16 @@ def tabla(numero):
 
 def area_cuadrado(lado):
     print(f"El area es: {lado*lado}")
+
+def operacion(num1, num2, op):
+    match op:
+        case 1:
+            print(f'La suma es: {num1+num2}')
+        case 2:
+            print(f'La resta es: {num1-num2}')
+        case 3:
+            print(f'La multiplicacion es: {num1*num2}')
+        case 4:
+            print(f'La division es: {num1/num2}')
+        case _:
+            print('Opción inválida.')
