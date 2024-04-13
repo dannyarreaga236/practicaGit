8a2026dc118eeed7917e96814de073a20308303a
Daniel Arreaga <d.arreaga236@gmail.com>
2024-04-12 23:24:31 -0600

    Prueba area cuadrado ok

--- pruebas.py
+++ pruebas.py
@@ -3,3 +3,7 @@ import ejercicio1 as ej1
 # Probar la tabla
 numero = int(input('Que tabla quieres? '))
 ej1.tabla(numero)
+
+# Probar el area del cuadrado
+lado = int(input('Cuanto mide el lado? '))
+ej1.area_cuadrado(lado)
